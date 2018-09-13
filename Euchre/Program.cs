9a041bab0d629e7671852d0c8f3ece4c1a4f5606
bdowds <bwdowds@gmail.com>
2018-09-13 08:51:11 -0500

    Add project files.

---
+++ Euchre/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Euchre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
